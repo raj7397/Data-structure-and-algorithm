51a10013da083b3e91909c3db26e4516941b43a5
Rajkalash Rajbhar <rajkalashrajbhar@gmail.com>
2021-12-02 13:24:10 +0530

    This is change

--- BitManipulation/Test3.java
+++ BitManipulation/Test3.java
@@ -2,6 +2,7 @@ public class Test3 {
     public static void main(String [] args)
     {
         System.out.println("This is test-3");
+	System.out.println("This is changed java pgm:");
 
         System.out.println("This another lin3");
     }
