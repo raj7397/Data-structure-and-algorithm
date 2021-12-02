fbadd845ecc85d99ebfb87cd0eb1cb62cfd52283
Rajkalash Rajbhar <rajkalashrajbhar@gmail.com>
2021-12-02 13:21:55 +0530

    Checking inial commit:e:wq!

---
+++ BitManipulation/BitManipulationQuestions.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class BitManipulationQuestions{
+    public static void main(String [] args)
+    {
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a=scn.nextInt();
+        System.out.println("a: "+a);
+    }
+}
